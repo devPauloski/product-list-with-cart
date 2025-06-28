745a225b18169eea5b3af3537ab9e987589a39f9
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-06-28 14:15:21 +0100

    Structure product list

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,32 @@
 import data from "../data.json";
 
 function ProductList() {
-  const products = data.map((product) => {
+  const products = data.map((item) => {
     return (
-      <li key={product.id}>
+      <li key={item.id}>
         <div>
-          <img src="" alt="" />
+          <picture>
+            <source srcSet={item.image.desktop} media="(min-width: 64em)" />
+            <source srcSet={item.image.tablet} media="(min-width: 48em)" />
+            <img src={item.image.mobile} alt={item.name} />
+          </picture>
+          <button>
+            <img src="/assets/images/icon-add-to-cart.svg" alt="" />
+            <span>Add to cart</span>
+          </button>
+          <div className="bg-rose-500">
+            <button aria-label="Increase quantity">
+              <img src="/assets/images/icon-increment-quantity.svg" alt="" />
+            </button>
+            <span aria-label="Quantity">0</span>
+            <button aria-label="Decrease quantity">
+              <img src="/assets/images/icon-decrement-quantity.svg" alt="" />
+            </button>
+          </div>
         </div>
+        <p>{item.category}</p>
+        <h2>{item.name}</h2>
+        <p>${item.price}</p>
       </li>
     );
   });
