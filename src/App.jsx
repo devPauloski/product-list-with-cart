93e5922edf9fd1d2d5de86af233790b144276f8e
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-06-28 18:21:01 +0100

    Make changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 import Product from "./components/Product.jsx";
 import Cart from "./components/Cart.jsx";
 import Order from "./components/Order.jsx";
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-
   return (
     <main className="max-w-[30rem] md:max-w-[76.25rem] mx-auto w-[90%] grid my-[1.875rem] md:grid-cols-[1fr_30%] gap-7">
       <Product />
