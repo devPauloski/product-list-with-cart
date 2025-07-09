b9445a257a1a33ebec33b78469cee452a80e8cd2
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-07-09 22:55:25 +0100

    Make changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,7 +4,7 @@ import carbonNeutralIcon from "../assets/icons/icon-carbon-neutral.svg";
 
 export default function Cart({ cartItems, deleteCartItem }) {
   return (
-    <section className="h-fit rounded-xl bg-white p-5 shadow-xl">
+    <section className="h-fit rounded-xl bg-white p-5 shadow-xl sticky top-[1.875rem]">
       <h2 className="mb-2 text-2xl leading-tight font-bold text-red">
         Your Cart ({cartItems.length})
       </h2>
@@ -22,7 +22,7 @@ export default function Cart({ cartItems, deleteCartItem }) {
               return (
                 <li
                   key={item.id}
-                  className="flex items-center justify-between border-b-1 border-solid border-b-rose-100 py-4"
+                  className="flex items-center justify-between border-b-1 border-solid border-b-rose-100 py-4 gap-2 flex-wrap"
                 >
                   <div>
                     <h3 className="text-sm font-bold text-rose-900">
@@ -49,11 +49,11 @@ export default function Cart({ cartItems, deleteCartItem }) {
               );
             })}
           </ul>
-          <div className="flex items-center justify-between my-4">
+          <div className="flex items-center justify-between my-4 flex-wrap">
             <p>Order Total</p>
             <p>total amount</p>
           </div>
-          <div className="flex items-center justify-center py-4 bg-rose-100 text-sm gap-2 mb-4 rounded-sm">
+          <div className="flex items-center justify-center py-4 bg-rose-100 text-sm gap-1 mb-4 rounded-sm flex-wrap">
             <img src={carbonNeutralIcon} alt="" />
             <p>This is a <span className="font-bold">carbon neutral</span> delivery</p>
           </div>
