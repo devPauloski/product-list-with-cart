cca78657f00f5328376d3970b0f66a0958da3459
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-07-10 20:30:37 +0100

    style order confirmation modal

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -4,44 +4,48 @@ export default function Order({ cartItems, isOrderConfirmed, startNewOrder, tota
   return (
     <>
       {isOrderConfirmed && (
-        <section className="fixed top-0 left-0 z-1 h-full w-full bg-overlay md:place-content-center">
-          <div className="mx-auto max-w-[37.125rem] rounded-xl bg-white px-6 pt-10 pb-6 max-md:mt-24">
+        <section className="fixed top-0 left-0 z-1 h-full w-full bg-overlay md:place-content-center overflow-auto">
+          <div className="mx-auto max-w-[37.125rem] rounded-xl bg-rose-50 px-[5%] pt-10 pb-6 max-md:mt-24 md:p-9">
             <OrderConfirmedIcon className="mb-8" />
             <h2 className="mb-4 text-4xl font-bold text-rose-900 max-md:max-w-[10ch]">
               Order Confirmed
             </h2>
             <p className="mb-6 text-rose-400">We hope you enjoy your food!</p>
-            <div className="bg-rose-100 p-5.5 rounded-lg">
+            <div className="rounded-lg bg-rose-100 p-5.5 mb-7.5">
               <ul>
                 {cartItems.map((item) => {
                   return (
                     <li
-                      className="flex items-center justify-between py-4"
+                      className="flex items-center justify-between gap-2 border-b border-rose-300 py-4"
                       key={item.id}
                     >
                       <div className="flex items-center gap-3">
-                        <img 
-                        className="w-12 h-12 rounded-sm"
-                          src={item.image.thumbnail} 
+                        <img
+                          className="h-12 w-12 rounded-sm"
+                          src={item.image.thumbnail}
                           alt={item.name}
-                         />
+                        />
                         <div>
-                          <h3>{item.name}</h3>
+                          <h3 className="text-sm font-bold">{item.name}</h3>
                           <div className="flex">
-                            <p className="mr-6 text-red">{item.quantity}x</p>
+                            <p className="mr-4 font-medium text-red">
+                              {item.quantity}x
+                            </p>
                             <p className="font-medium text-rose-500">
                               &#64;${item.price}
                             </p>
                           </div>
                         </div>
                       </div>
-                      <p>${item.price * item.quantity}</p>
+                      <p className="font-medium">
+                        ${(item.price * item.quantity).toFixed(2)}
+                      </p>
                     </li>
                   );
                 })}
               </ul>
-              <div className="flex">
-                <p>Order Total</p>
+              <div className="flex items-center justify-between mt-4">
+                <p className="font-medium">Order Total</p>
                 <p className="text-2xl font-bold">
                   ${totalAmount().toFixed(2)}
                 </p>
@@ -51,6 +55,7 @@ export default function Order({ cartItems, isOrderConfirmed, startNewOrder, tota
               onClick={() => {
                 startNewOrder();
               }}
+              className="w-full h-14 bg-red cursor-pointer flex items-center justify-center px-4 hover:bg-red-100 rounded-full text-rose-50 font-medium"
             >
               Start New Order
             </button>
